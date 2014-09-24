c17e77e05ca29a89b159ebd4cfa582d4db7ba039
Laurent Goderre <laurent.goderre@gmail.com>
2014-09-24 17:40:13 -0400

    Added the module's logic

--- test/core.js
+++ test/core.js
@@ -1,10 +1,44 @@
 var expect = require('expect.js');
+var ncp = require('ncp').ncp;
+var rimraf = require('rimraf');
+var git = require('../lib/helpers/git');
 var release = require('../lib/release');
 
 describe('Core Release Process', function () {
+	var testFolder = './test',
+		initialFolder = testFolder + '/initial',
+		originFolder = testFolder + '/origin',
+		upstreamFolder = testFolder + '/upstream',
+		originPath = originFolder + '/core',
+		origin = new git(originPath),
+		upstreamPath = upstreamFolder + '/core',
+		upstream = new git(upstreamPath);
 
-	before(function() {
-		release();
+	before(function( done ) {
+		ncp(initialFolder, originFolder, function(err) {
+			if (err) {
+				return console.error(err);
+			}
+
+			ncp(initialFolder, upstreamFolder, function(err) {
+				if (err) {
+					return console.error(err);
+				}
+
+				upstream.create()
+				.then(function(){
+					return origin.create();
+				})
+				.then(function(){
+					release(originPath, done);
+				});
+			});
+		});
+	});
+
+	after(function(){
+		rimraf.sync(originFolder);
+		rimraf.sync(upstreamFolder);
 	});
 
 	it('runs', function() {
