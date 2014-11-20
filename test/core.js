1f974d89b20c6027ea5e2dd70f6195fe10b4d407
Laurent Goderre <laurent.goderre@gmail.com>
2014-11-20 14:55:12 -0500

    Added support for metadata releases

--- test/core.js
+++ test/core.js
@@ -33,144 +33,319 @@ describe('Core Release Process', function () {
 			return origin.exec('push', upstream.cwd, 'master');
 		}, error)
 		.then(function() {
-			var deferred = Q.defer(),
-			deferedCallback = function(err) {
-				if (err) {
-					return deferred.reject(err);
-				}
-
-				return deferred.resolve();
-			};
-			r = release(origin, deferedCallback, false);
-
-			return deferred.promise;
-		}, error)
-		.then(function() {
-			return origin.exec('branch');
-		}, error).then(function(repo) {
-			localBranchName = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
 			callback();
 		}, error);
 	});
 
-	it('Creates a commit for the new version', function(done) {
-		callback = done;
+	describe('Main version', function() {
+		before(function(done) {
+			var deferred = Q.defer(),
+				deferedCallback = function(err) {
+					if (err) {
+						return deferred.reject(err);
+					}
 
-		origin.exec('log', localBranchName + '~2..' + localBranchName +'~1')
-		.then(function(repo) {
-			expect(repo.lastCommand.stdout).to.contain('Updated files for the v4.0.1 maintenance release');
+					return deferred.resolve();
+				};
+				r = release(origin, deferedCallback, false);
 
-			callback();
-		}, error)
-		.then(null, error);
-	});
+			callback = done;
 
-	it('Updates the version number in package.json and bower.json for the release', function(done) {
-		callback = done;
+			deferred.promise
+			.then(function() {
+				return origin.exec('branch');
+			}, error).then(function(repo) {
+				localBranchName = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
+				callback();
+			}, error);
+		});
 
-		origin.exec('rev-parse', localBranchName + '~1')
-		.then(function(repo) {
-			var commitNumber = repo.lastCommand.stdout.replace('\n', '');
+		it('Creates a commit for the new version', function(done) {
+			callback = done;
 
-			return origin.exec('checkout', commitNumber);
-		}, error)
-		.then(function() {
-			var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
-				bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
-
-			expect(pkg.version).to.be('4.0.1');
-			expect(bower.version).to.be('4.0.1');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Creates a tag for the release', function(done) {
-		callback = done;
-
-		origin.exec('tag', '-l', '-n')
-		.then(function(repo) {
-			expect(repo.lastCommand.stdout).to.contain('v4.0.1');
-			expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1 maintenance release');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Creates a commit for the new development version', function(done) {
-		callback = done;
-
-		origin.exec('log', localBranchName + '~1..' + localBranchName)
-		.then(function(repo) {
-			expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Updates the version number in package.json and bower.json for the development version', function(done) {
-		callback = done;
-
-		origin.exec('rev-parse', localBranchName)
-		.then(function(repo) {
-			var commitNumber = repo.lastCommand.stdout.replace('\n', '');
-
-			return origin.exec('checkout', commitNumber);
-		}, error)
-		.then(function() {
-			var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
-				bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
-
-			expect(pkg.version).to.be('4.0.2-development');
-			expect(bower.version).to.be('4.0.2-development');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Pushes the release branch upstream', function(done) {
-		callback = done;
-
-		upstream.exec('log', 'master')
-		.then(function(repo) {
-			expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Pushes the tag upstream', function(done) {
-		callback = done;
-
-		upstream.exec('tag', '-l', '-n')
-		.then(function(repo) {
-			expect(repo.lastCommand.stdout).to.contain('v4.0.1');
-			expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1 maintenance release');
-
-			callback();
-		}, error)
-		.then(null, error);
-	});
-
-	it('Cleans up after the release', function(done) {
-		callback = done;
-
-		r.cleanup(function() {
-			return origin.exec('branch')
+			origin.exec('log', localBranchName + '~2..' + localBranchName +'~1')
 			.then(function(repo) {
-				var branch = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
-
-				expect(branch).to.be('master');
-				expect(repo.lastCommand.stdout).to.not.contain(localBranchName);
+				expect(repo.lastCommand.stdout).to.contain('Updated files for the v4.0.1 maintenance release');
 
 				callback();
 			}, error)
 			.then(null, error);
+		});
+
+		it('Updates the version number in package.json and bower.json for the release', function(done) {
+			callback = done;
+
+			origin.exec('rev-parse', localBranchName + '~1')
+			.then(function(repo) {
+				var commitNumber = repo.lastCommand.stdout.replace('\n', '');
+
+				return origin.exec('checkout', commitNumber);
+			}, error)
+			.then(function() {
+				var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
+					bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
+
+				expect(pkg.version).to.be('4.0.1');
+				expect(bower.version).to.be('4.0.1');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Creates a tag for the release', function(done) {
+			callback = done;
+
+			origin.exec('tag', '-l', '-n')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('v4.0.1');
+				expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1 maintenance release');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Creates a commit for the new development version', function(done) {
+			callback = done;
+
+			origin.exec('log', localBranchName + '~1..' + localBranchName)
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Updates the version number in package.json and bower.json for the development version', function(done) {
+			callback = done;
+
+			origin.exec('rev-parse', localBranchName)
+			.then(function(repo) {
+				var commitNumber = repo.lastCommand.stdout.replace('\n', '');
+
+				return origin.exec('checkout', commitNumber);
+			}, error)
+			.then(function() {
+				var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
+					bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
+
+				expect(pkg.version).to.be('4.0.2-development');
+				expect(bower.version).to.be('4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Pushes the release branch upstream', function(done) {
+			callback = done;
+
+			upstream.exec('log', 'master')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Pushes the tag upstream', function(done) {
+			callback = done;
+
+			upstream.exec('tag', '-l', '-n')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('v4.0.1');
+				expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1 maintenance release');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Cleans up after the release', function(done) {
+			callback = done;
+
+			r.cleanup(function() {
+				return origin.exec('branch')
+				.then(function(repo) {
+					var branch = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
+
+					expect(branch).to.be('master');
+					expect(repo.lastCommand.stdout).to.not.contain(localBranchName);
+
+					callback();
+				}, error)
+				.then(null, error);
+			});
+		});
+	});
+
+	describe('Metadata version', function() {
+		before(function(done){
+			callback = done;
+
+			origin.exec('pull', upstream.cwd, 'master')
+			.then(function(repo) {
+				var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
+					bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json'))),
+					metadataVersion = '4.0.1+1-development';
+
+				pkg.version = metadataVersion;
+				bower.version = metadataVersion;
+
+				fs.writeFileSync(path.join(origin.cwd + '/package.json'), JSON.stringify(pkg, null, 2));
+				fs.writeFileSync(path.join(origin.cwd + '/bower.json'), JSON.stringify(bower, null, 2));
+
+				return origin.exec('add', '.');
+			}, error)
+			.then(function(repo){
+				return repo.exec('commit', '-m', 'Updated version for metadata release');
+			},error)
+			.then(function(repo){
+				return repo.exec('push', upstream.cwd, 'master');
+			}, error)
+			.then(function(){
+				var deferred = Q.defer(),
+				deferedCallback = function(err) {
+					if (err) {
+						return deferred.reject(err);
+					}
+
+					return deferred.resolve();
+				};
+				r = release(origin, deferedCallback, false);
+
+				return deferred.promise;
+			})
+			.then(function() {
+				return origin.exec('branch');
+			}, error).then(function(repo) {
+				localBranchName = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
+				callback();
+			}, error);
+			});
+
+		it('Creates a commit for the new version', function(done) {
+			callback = done;
+
+			origin.exec('log', localBranchName + '~2..' + localBranchName +'~1')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('Updated files for the v4.0.1+1 maintenance release');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Updates the version number in package.json and bower.json for the release', function(done) {
+			callback = done;
+
+			origin.exec('rev-parse', localBranchName + '~1')
+			.then(function(repo) {
+				var commitNumber = repo.lastCommand.stdout.replace('\n', '');
+
+				return origin.exec('checkout', commitNumber);
+			}, error)
+			.then(function() {
+				var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
+					bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
+
+				expect(pkg.version).to.be('4.0.1+1');
+				expect(bower.version).to.be('4.0.1+1');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Creates a tag for the release', function(done) {
+			callback = done;
+
+			origin.exec('tag', '-l', '-n')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('v4.0.1+1');
+				expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1+1 maintenance release');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Creates a commit for the new development version', function(done) {
+			callback = done;
+
+			origin.exec('log', localBranchName + '~1..' + localBranchName)
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Updates the version number in package.json and bower.json for the development version', function(done) {
+			callback = done;
+
+			origin.exec('rev-parse', localBranchName)
+			.then(function(repo) {
+				var commitNumber = repo.lastCommand.stdout.replace('\n', '');
+
+				return origin.exec('checkout', commitNumber);
+			}, error)
+			.then(function() {
+				var pkg = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/package.json'))),
+					bower = JSON.parse(fs.readFileSync(path.join(origin.cwd + '/bower.json')));
+
+				expect(pkg.version).to.be('4.0.2-development');
+				expect(bower.version).to.be('4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Pushes the release branch upstream', function(done) {
+			callback = done;
+
+			upstream.exec('log', 'master')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('Updated the build version to v4.0.2-development');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Pushes the tag upstream', function(done) {
+			callback = done;
+
+			upstream.exec('tag', '-l', '-n')
+			.then(function(repo) {
+				expect(repo.lastCommand.stdout).to.contain('v4.0.1');
+				expect(repo.lastCommand.stdout).to.contain('Source files for the v4.0.1 maintenance release');
+
+				callback();
+			}, error)
+			.then(null, error);
+		});
+
+		it('Cleans up after the release', function(done) {
+			callback = done;
+
+			r.cleanup(function() {
+				return origin.exec('branch')
+				.then(function(repo) {
+					var branch = repo.lastCommand.stdout.match(currentBranchRegexp)[1];
+
+					expect(branch).to.be('master');
+					expect(repo.lastCommand.stdout).to.not.contain(localBranchName);
+
+					callback();
+				}, error)
+				.then(null, error);
+			});
 		});
 	});
 });
